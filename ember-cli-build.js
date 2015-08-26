c981c936a0bf3f035148bcba844a234a5de6ab7a
Mudragada, Krishna <v-mudrak-8L@ae.com>
2015-08-25 23:32:34 -0400

    First Commit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -18,6 +18,12 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.ttf', {
+   destDir: 'assets'
+ });
 
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/slider-revolution/src/js/jquery.themepunch.revolution.min.js')
   return app.toTree();
 };
